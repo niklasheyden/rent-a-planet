653d7e41a0823e97ebfcd4f496b2d490eca9f495
Manal Malik <manalmalik.cs@gmail.com>
2020-08-17 17:18:37 +0200

    Seed data to the db models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+Planet.destroy_all
+User.destroy_all
+
+5.times do
+  user = User.create!(
+      {
+        name: Faker::FunnyName.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password
+      }
+    )
+  p user
+end
+
+
+User.all.each do |user|
+  rand(1..4).times do
+
+    planet = Planet.create!(
+      {
+        name: Faker::Space.galaxy,
+        location: Faker::Address.street_address,
+        price: rand(5000..15_000),
+        user_id: user.id
+      }
+    )
+  end
+ # planet.user = user
+
+
+end
+
+
