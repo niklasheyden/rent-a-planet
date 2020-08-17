1794d58337b692780857caed5c5851f34d788f64
Manal Malik <manalmalik.cs@gmail.com>
2020-08-17 18:15:05 +0200

    Add devise to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only:[:new, :create, :edit, :destroy] do
-    resources :planets, only: [:index, :new, :create, :show] do
-      resources :bookings, only: [:show, :new, :create, :destroy]
-    end
-  end
+  # resources :users, only:[:new, :create, :edit, :destroy] do
+  #   resources :planets, only: [:index, :new, :create, :show] do
+  #     resources :bookings, only: [:show, :new, :create, :destroy]
+  #   end
+  # end
 end
