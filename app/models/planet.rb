dabc942c890e66f5b96c1783533f769caa8c5b18
Manal Malik <manalmalik.cs@gmail.com>
2020-08-17 15:41:21 +0200

    Add db associations to models

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,5 @@
 class Planet < ApplicationRecord
+  has_many :bookings
+  belongs_to :users
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :bookings
+  has_many :planets
+  has_many :planets, through: :bookings
+  has_many :reviews
 end
