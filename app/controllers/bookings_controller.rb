9bc6359ce3fc3ffd8a7f264003c0631088f36638
Manal Malik <manalmalik.cs@gmail.com>
2020-08-18 16:30:03 +0200

    Add the booking feature for the app

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,28 @@
 class BookingsController < ApplicationController
 
   def new
+    #raise
     @booking = Booking.new
+    @planet = Planet.find(params[:planet_id])
     authorize @booking
   end
 
   def create
-    @booking = Booking.create(booking_params)
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @planet = Planet.find(params[:planet_id])
+    @booking.planet = @planet
     authorize @booking
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to planet_booking_path(@planet, @booking)
     else
       render :new
     end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
   end
 
   private
