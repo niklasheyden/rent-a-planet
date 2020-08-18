ee9d551bb2ea58ee0fecb73bd6aa5872b870fe0e
Manal Malik <manalmalik.cs@gmail.com>
2020-08-18 10:50:29 +0200

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :planets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :users, only:[:new, :create, :edit, :destroy] do
   #   resources :planets, only: [:index, :new, :create, :show] do
