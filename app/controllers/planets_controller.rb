8cab7310e5eff3404e1fe63615fd439cbb38c4f2
Niklas von Heyden <niklas.heyden@me.com>
2020-08-18 14:41:56 +0200

    create new planet feature

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,23 +1,24 @@
 class PlanetsController < ApplicationController
 before_action :set_planet, only: [:show]
-def index
-  @planets = policy_scope(Planet)
-end
-
-def new
-  @planet = Planet.new
-  authorize @planet
-end
-
-def create
-  @planet = Planet.new(planet_params)
-  authorize @planet
-  if @planet.save
-    redirect_to planet_path(@planet)
-  else
-    render :new
+  def index
+    @planets = policy_scope(Planet)
   end
-end
+
+  def new
+    @planet = Planet.new
+    authorize @planet
+  end
+
+  def create
+    @planet = Planet.new(planet_params)
+    @planet.user = current_user
+    authorize @planet
+    if @planet.save
+      redirect_to planet_path(@planet), notice: 'Planet was successfully created.'
+    else
+      render :new
+    end
+  end
 
   def show
   end
