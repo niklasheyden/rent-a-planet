4c0f2077aa85fe8f9189822b51b97053c0740da6
Arthur Bizet <arthur.m.bizet@gmail.com>
2020-08-20 18:18:22 +0200

    search front end

--- app/javascript/plugins/search_form.js
+++ app/javascript/plugins/search_form.js
@@ -1,18 +1,8 @@
-// const form = document.querySelector(".search-form");
-// function handleForm (event) {
-//   $('.cards-planet').load(document.URL + '.cards-planet');
-//   // event.preventDefault();
-// };
+const form = document.querySelector(".search-form");
+function handleForm (event) {
+  $('.cards-planet').load(document.URL + '.cards-planet');
+  // event.preventDefault();
+};
 
-// form.addEventListener('submit', handleForm);
+form.addEventListener('submit', handleForm);
 
-let sidebar = document.querySelector(".sidebar");
-
-let top = localStorage.getItem("sidebar-scroll");
-if (top !== null) {
-  sidebar.scrollTop = parseInt(top, 10);
-}
-
-window.addEventListener("beforeunload", () => {
-  localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
-});
